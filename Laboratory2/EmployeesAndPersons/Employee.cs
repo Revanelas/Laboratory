6116ef713a784dffa9ddc60b3a9be04883ea2625
Revanelas <66388808+Revanelas@users.noreply.github.com>
2020-06-03 22:47:23 +0200

    Add files via upload

---
+++ Laboratory2/EmployeesAndPersons/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesAndPersons
+{
+    public class Employee : Person
+    {
+        private string password;
+
+        new public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (value.Length >= 6)
+                {
+                    password = value;
+                }
+                else
+                {
+                    throw new System.ArgumentException("Password is too short. Min. 6 characters are required", "Password");
+                };
+            }
+        }
+
+
+        public string Department { get; set; }
+
+
+        public void AddPerson(string login, string password, string name, string department)
+        {
+            this.Login = login;
+            this.Password = password;
+            this.Name = name;
+            this.Department = department;
+        }
+    }
+}
---
+++ Laboratory2/EmployeesAndPersons/RandomString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesAndPersons
+{
+    public class RandomString
+    {
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+
+        public string RandomStrings()
+        {
+            int length = random.Next(6, 32);
+            char[] rnd=new char[length];
+
+            for (int i = 0; i < rnd.Length; i++)
+            {
+                rnd[i] = chars[random.Next(chars.Length)];
+            }
+
+            string solution = new string(rnd);
+
+            return solution;
+        }
+    }
+}
